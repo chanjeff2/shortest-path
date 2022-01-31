ba6897cb539250ea4015b6f4c34a7e804d188000
chanjeff <jeffchan142@gmail.com>
2022-01-31 16:52:09 +0800

    fix: getComputedStyle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,16 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import { createTheme, ThemeProvider as MuiThemeProvider } from '@mui/material'
 
-const theme = createTheme({
-  palette: {
-    text: {
-      primary: getComputedStyle(document.documentElement).getPropertyValue('--foreground'),
-      secondary: `rgba(${getComputedStyle(document.documentElement).getPropertyValue('--foreground')}, 0.87)`,
-    }
-  }
-})
-
 function MyApp({ Component, pageProps }: AppProps) {
+  const theme = createTheme({
+    palette: {
+      text: {
+        primary: getComputedStyle(document.documentElement).getPropertyValue('--foreground'),
+        secondary: `rgba(${getComputedStyle(document.documentElement).getPropertyValue('--foreground')}, 0.87)`,
+      }
+    }
+  })
+
   return (
     <ThemeProvider>
       <MuiThemeProvider theme={theme}>
