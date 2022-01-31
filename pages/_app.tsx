72e92e54f0efcbea86dc573545ccffafafdbd2c5
chanjeff <jeffchan142@gmail.com>
2022-01-31 16:48:58 +0800

    fix: MUI theme color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
+import { createTheme, ThemeProvider as MuiThemeProvider } from '@mui/material'
+
+const theme = createTheme({
+  palette: {
+    text: {
+      primary: getComputedStyle(document.documentElement).getPropertyValue('--foreground'),
+      secondary: `rgba(${getComputedStyle(document.documentElement).getPropertyValue('--foreground')}, 0.87)`,
+    }
+  }
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
-      <Component {...pageProps} />
+      <MuiThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </MuiThemeProvider>
     </ThemeProvider>
   )
 }
