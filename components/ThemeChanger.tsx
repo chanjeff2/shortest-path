da2d33edf110475398a32defd371b03248311119
chanjeff <jeffchan142@gmail.com>
2022-01-31 22:45:01 +0800

    fix: mui theme

--- components/ThemeChanger.tsx
+++ components/ThemeChanger.tsx
@@ -1,10 +1,12 @@
 import { FormControlLabel, Switch } from '@mui/material'
 import { useTheme } from 'next-themes'
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
+import { ColorModeContext } from '../pages/_app'
 
 export const ThemeChanger = () => {
     const [mounted, setMounted] = useState(false)
     const { theme, setTheme } = useTheme()
+    const colorMode = useContext(ColorModeContext);
 
     // When mounted on client, now we can show the UI
     useEffect(() => setMounted(true), [])
@@ -12,6 +14,17 @@ export const ThemeChanger = () => {
     if (!mounted) return null
 
     return (
-        <FormControlLabel control={<Switch defaultChecked onChange={event => setTheme(event.target.checked ? "dark" : "light")} />} label="Dark Mode" />
+        <FormControlLabel
+            label="Dark Mode"
+            control={
+                <Switch
+                    defaultChecked
+                    onChange={event => {
+                        setTheme(event.target.checked ? "dark" : "light")
+                        colorMode.toggleColorMode()
+                    }}
+                />
+            }
+        />
     )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,39 +2,67 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import { createTheme, Theme, ThemeProvider as MuiThemeProvider } from '@mui/material'
-import { useEffect, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
+
+interface ColorModeContextInterface {
+  toggleColorMode: () => void
+}
+
+export const ColorModeContext = createContext<ColorModeContextInterface>({ toggleColorMode: () => { } });
+
+function getCssVar(property: string): string {
+  return window.getComputedStyle(document.documentElement).getPropertyValue(property)
+}
+
+function getMuiTheme(): Theme {
+  return createTheme({
+    palette: {
+      text: {
+        primary: getCssVar('--foreground'),
+        secondary: `rgba(${getCssVar('--foreground-inline')}, 0.7)`,
+        disabled: `rgba(${getCssVar('--foreground-inline')}, 0.4)`,
+      },
+      action: {
+        active: `rgba(${getCssVar('--foreground-inline')}, 0.5)`,
+        hover: `rgba(${getCssVar('--foreground-inline')}, 0.04)`,
+        selected: `rgba(${getCssVar('--foreground-inline')}, 0.12)`,
+        disabled: `rgba(${getCssVar('--foreground-inline')}, 0.3)`,
+        disabledBackground: `rgba(${getCssVar('--foreground-inline')}, 0.12)`,
+      },
+      background: {
+        default: getCssVar('--background'),
+        paper: getCssVar('--background'),
+      },
+      divider: `rgba(${getCssVar('--foreground-inline')}, 0.12)`,
+    }
+  })
+}
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [theme, setTheme] = useState<Theme | null>(null);
-  useEffect(() => {
-    let theme = createTheme({
-      palette: {
-        text: {
-          primary: window.getComputedStyle(document.documentElement).getPropertyValue('--foreground'),
-          secondary: `rgba(${window.getComputedStyle(document.documentElement).getPropertyValue('--foreground')}, 0.87)`,
-        }
-      }
-    })
-    setTheme(theme)
-  })
-
-  if (theme) {
-    return (
-      <ThemeProvider>
-        <MuiThemeProvider theme={theme}>
-          <Component {...pageProps} />
-        </MuiThemeProvider>
-      </ThemeProvider>
-    )
-  } else {
-    return (
-      <ThemeProvider>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    )
+  const [muiTheme, setMuiTheme] = useState<Theme | null>(null);
+  const colorMode: ColorModeContextInterface = {
+    toggleColorMode: () => {
+      let theme = getMuiTheme()
+      setMuiTheme(theme)
+    }
   }
 
+  useEffect(() => {
+    colorMode.toggleColorMode()
+  }, [])
 
+  if (!muiTheme) return null
+
+  return (
+    <ThemeProvider>
+      <ColorModeContext.Provider value={colorMode}>
+        <MuiThemeProvider theme={muiTheme}>
+          <Component {...pageProps} />
+        </MuiThemeProvider>
+      </ColorModeContext.Provider>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
